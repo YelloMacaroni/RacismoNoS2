6c28747c860e1cc3726c36a2b5ac98f8c542982c
YelloMacaroni <lucas.ly-ba@epita.fr>
2023-03-21 22:38:27 +0100

    oe

--- RacismoNo/Assets/Audio/scriptmusique.cs
+++ RacismoNo/Assets/Audio/scriptmusique.cs
@@ -1,30 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
-public class scriptmusique : MonoBehaviour
-{
-    public AudioClip sound;
-    private AudioSource source;
-    void Awake()
-    {
-        gameObject.AddComponent<AudioSource>();
-        source= GetComponent<AudioSource>();
-    }
-    void start()
-    {
-        source.clip=sound;
-    }
-    void Update()
-    {
-        if(Input.GetKeyDown("space"))
-            dd();
-    }
-    private void dd()
-    {
-        if(!source.isPlaying)
-            source.Play();
-        else 
-            source.Pause();
-    }
+public class scriptmusique : MonoBehaviour {
+	
+	public bool keepPlaying = false;
+	public static scriptmusique NSPlay;
+	
+	void Awake()
+	{
+		if(NSPlay == null)
+		{
+			NSPlay = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else
+		{
+			var audioSource = NSPlay.GetComponent<AudioSource>();
+			if(keepPlaying)
+			{
+				if(!audioSource.isPlaying)
+				{
+					audioSource.Play();
+				}
+			}
+			else
+			{
+				if(audioSource.isPlaying)
+				{
+					audioSource.Stop();
+				}
+			}
+			Destroy(gameObject);
+		}
+	}
 }
