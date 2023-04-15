8a2c51f17d6bdd0c0746c54d95384aba568c4066
YelloMacaroni <lucas.ly-ba@epita.fr>
2023-04-15 23:38:51 +0200

    ddd
    dddddd

---
+++ RacismoNo/Assets/Scripts/invisiblebody.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+
+public class invisiblebody : MonoBehaviourPunCallbacks
+{
+    Renderer ren;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ren=GetComponent<Renderer>();
+        if (photonView.IsMine)
+            ren.enabled=false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
