9fd9a9fd8798ce5d4000edbedf916b2c13b81b36
Torakle <113019792+Torakle@users.noreply.github.com>
2023-04-16 22:13:33 +0200

    Bonjour

--- RacismoNo/Assets/Scripts/Code/PlayerControls/MultiMove.cs
+++ RacismoNo/Assets/Scripts/Code/PlayerControls/MultiMove.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using UnityEngine.UI;
-
+//BONJOUR 
 public class MultiMove : MonoBehaviourPunCallbacks
 {
     Animator animator;
