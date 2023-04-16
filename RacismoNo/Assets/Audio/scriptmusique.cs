41a88769e23f6656f1d1026470924ff9a735acd1
YelloMacaroni <lucas.ly-ba@epita.fr>
2023-04-16 21:35:32 +0200

    qds
    qdsqsdqsd

--- RacismoNo/Assets/Audio/scriptmusique.cs
+++ RacismoNo/Assets/Audio/scriptmusique.cs
@@ -1,4 +1,4 @@
-<<<<<<< Updated upstream
+
 using UnityEngine;
 using System.Collections;
 
@@ -35,35 +35,3 @@ public class scriptmusique : MonoBehaviour {
 		}
 	}
 }
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class scriptmusique : MonoBehaviour
-{
-    public AudioClip sound;
-    private AudioSource source;
-    void Awake()
-    {
-        gameObject.AddComponent<AudioSource>();
-        source= GetComponent<AudioSource>();
-    }
-    void start()
-    {
-        source.clip=sound;
-    }
-    void Update()
-    {
-        if(Input.GetKeyDown("space"))
-            dd();
-    }
-    private void dd()
-    {
-        if(!source.isPlaying)
-            source.Play();
-        else 
-            source.Pause();
-    }
-}
->>>>>>> Stashed changes
