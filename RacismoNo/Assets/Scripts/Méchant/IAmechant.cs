0f863568356613e0fe32b57cc75a92d7a1d8a646
elbitor0 <123994826+elbitor0@users.noreply.github.com>
2023-04-17 19:16:37 +0200

    IA patrouille et va vers le joueurs si vu
    sa marche

--- RacismoNo/Assets/Scripts/Méchant/IAmechant.cs
+++ RacismoNo/Assets/Scripts/Méchant/IAmechant.cs
@@ -1,22 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
-namespace Méchant
+using UnityEngine.AI; //important
+
+//if you use this code you are contractually obligated to like the YT video
+public class IAmechant : MonoBehaviour //don't forget to change the script name if you haven't
 {
-    public class IAmechant : MonoBehaviour
+    public NavMeshAgent agent;
+    public float range; //radius of sphere
+
+    [SerializeField]
+    public Transform cam;
+    [SerializeField] private bool active = false;
+    [SerializeField] public float agrodDistance;
+    public Transform centrePoint; //centre of the area the agent wants to move around in
+    //instead of centrePoint you can set it as the transform of the agent if you don't care about a specific area
+
+    void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
+    }
 
-        public Camera cam;
+    
+    void Update()
+    {
+        if(agent.remainingDistance <= agent.stoppingDistance) //done with path
+        {
+            Vector3 point;
+            if (RandomPoint(centrePoint.position, range, out point)) //pass in our centre point and radius of area
+            {
+                Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f); //so you can see with gizmos
+                agent.SetDestination(point);
+            }
+        }
 
-        private NavMeshAgent agent;
-        // Start is called before the first frame update
-        void Start()
+        if (SetAgro())
         {
             
         }
 
-        // Update is called once per frame
-        
-
-        
     }
+    bool RandomPoint(Vector3 center, float range, out Vector3 result)
+    {
+
+        Vector3 randomPoint = center + Random.insideUnitSphere * range; //random point in a sphere 
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas)) //documentation: https://docs.unity3d.com/ScriptReference/AI.NavMesh.SamplePosition.html
+        { 
+            //the 1.0f is the max distance from the random point to a point on the navmesh, might want to increase if range is big
+            //or add a for loop like in the documentation
+            result = hit.position;
+            return true;
+        }
+
+        result = Vector3.zero;
+        return false;
+    }
+
+    bool SetAgro()
+    {
+        RaycastHit hit;
+        active = Physics.Raycast(cam.position,cam.TransformDirection(Vector3.forward),out hit,agrodDistance);
+        if (active && hit.transform.CompareTag("Player"))
+        {
+            agent.speed = 7;
+            agent.SetDestination(hit.transform.position);
+            return true;
+        }
+        agent.speed = (float)3.5;
+        return false;
+
+    }
+
+    
 }
