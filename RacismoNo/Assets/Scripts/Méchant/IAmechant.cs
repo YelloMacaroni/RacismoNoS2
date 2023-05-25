564509a5cabbe4f1de7b84f2f5de52c1529e8922
elbitor0 <123994826+elbitor0@users.noreply.github.com>
2023-05-25 23:16:01 +0200

    IA fix et ajustement
    l'ia marchent les portes du 1 aussi il faut juste savoir ou mettre les clés, et quelque ajustements au 1

--- RacismoNo/Assets/Scripts/Méchant/IAmechant.cs
+++ RacismoNo/Assets/Scripts/Méchant/IAmechant.cs
@@ -13,8 +13,6 @@ public class IAmechant : MonoBehaviour //don't forget to change the script name 
     public Transform cam;
     [SerializeField] private bool active = false;
     [SerializeField] public float agrodDistance;
-    public Transform centrePoint; //centre of the area the agent wants to move around in
-    //instead of centrePoint you can set it as the transform of the agent if you don't care about a specific area
 
     void Start()
     {
@@ -27,21 +25,19 @@ public class IAmechant : MonoBehaviour //don't forget to change the script name 
         if(agent.remainingDistance <= agent.stoppingDistance) //done with path
         {
             Vector3 point;
-            if (RandomPoint(centrePoint.position, range, out point)) //pass in our centre point and radius of area
+            if (RandomPoint(agent.transform.position, out point)) //pass in our centre point and radius of area
             {
                 Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f); //so you can see with gizmos
                 agent.SetDestination(point);
             }
         }
 
-         if (SetAgro())
-        {
+        SetAgro();
 
-        }
-        
 
     }
-    bool RandomPoint(Vector3 center, float range, out Vector3 result)
+
+    bool RandomPoint(Vector3 center,  out Vector3 result)
     {
 
         Vector3 randomPoint = center + Random.insideUnitSphere * range; //random point in a sphere 
